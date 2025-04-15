2b933b7c0de472e0ea9236fff76151d6bea41a38
dornierv <dorniervincent0@gmail.com>
2025-04-15 15:17:50 +0200

    New version

--- NEUREQUA/__init__.py
+++ NEUREQUA/__init__.py
@@ -4,7 +4,7 @@ NeuReQua
 Python library to monitor your signal quality
 """
 
-__version__ = "0.1.1.16"
+__version__ = "0.1.1.17"
 __author__ = 'Vincent Dornier'
 __credits__ = 'Centre National de la Recherche Scientifique (CNRS)'
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='neurequa',
 
-    version='0.1.1.16',    
+    version='0.1.1.17',    
 
     description='Python package to monitor micro-recording quality in humans',
     url='https://github.com/dornierv/NEUREQUA',
