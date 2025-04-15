514c047d17cc21b8229f8430de9dea0720049f6e
dornierv <dorniervincent0@gmail.com>
2025-04-15 08:57:08 +0200

    New version
    Can choose the name of reference channel in blackrock in load_data

--- NEUREQUA/quality_module.py
+++ NEUREQUA/quality_module.py
@@ -13,7 +13,7 @@ import scipy.stats as stats
 import matplotlib
 
 
-def load_raw_data(path,dtype,length,time='Random',*args):
+def load_raw_data(path,dtype,length,time='Random',analog='ainp1',*args):
     """
     Load data from the raw files (e.g., .ncs for Neuralynx)
 
@@ -41,6 +41,9 @@ def load_raw_data(path,dtype,length,time='Random',*args):
         By default take randomly portion of signal corresponding to length
         It can be a tuple (2,7) for example to take signal from 2 minutes to 7 minutes of the recording
     
+    analog: string (optional)
+        Reference channel used in blackrock recording system (default value : ainp1)
+    
     
     Returns
     ---------------------------
@@ -60,7 +63,7 @@ def load_raw_data(path,dtype,length,time='Random',*args):
     elif dtype=='nsX':
         raw_data = mne.io.read_raw_nsx(path)
         # Exclude the reference channel
-        raw_data.drop_channels('ainp1')
+        raw_data.drop_channels(analog)
     elif dtype=='dat':
         # Here not sure everyone is using int16 but for us it is ok
         data_type = np.int16
