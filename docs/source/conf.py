21ee6b990d9777882742ad77ed6370296789cb82
dornierv <dorniervincent0@gmail.com>
2026-06-05 16:37:17 +0200

    autoapi

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,8 +14,7 @@ release = '0.1..1.19'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-]
+extensions = ['autoapi.extension']
 
 templates_path = ['_templates']
 exclude_patterns = []
