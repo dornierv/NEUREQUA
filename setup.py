8f5dc2f93238b85c151d04f5244ae2d9f8c91449
dornierv <dorniervincent0@gmail.com>
2026-07-15 13:20:13 +0200

    New version pip

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='neurequa',
 
-    version='0.1.1.19',    
+    version='0.1.1.20',    
 
     description='Python package to monitor micro-recording quality in humans',
     url='https://github.com/dornierv/NEUREQUA',
